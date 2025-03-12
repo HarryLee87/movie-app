c0e6e0f30468c9806bbdf3c34171d3a81f898eb4
Harry Lee <haewon.harry.lee@gmail.com>
2025-03-11 23:49:00 -0600

    styled button css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import Button from "./Button";
+import "./styles.css"
+
 function App() {
   return (
     <div>
-      <h1>Hello, world!</h1>
+      <h1>Hello, world!!</h1>
+      <Button text="Button" />
     </div>
   );
 }
